0ecf9454182534637ec6429bc04fc52b366bc43d
donut2008 <76439683+donut2008@users.noreply.github.com>
2021-12-01 20:00:09 +0530

    upload

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+	import Header from '$lib/header/Header.svelte';
+	import '../app.css';
+	import * as Fluent from "fluent-svelte";
+	let dialogOpen = false;
+</script>
+
+<Header />
+
+<main>
+	<slot/>
+</main>
+
+<footer>
+	<div class="pad">
+		<Fluent.Expander>GitHub projects
+			<svelte:fragment slot="content">
+				<ul>
+					<li><Fluent.Button variant="hyperlink" href="https://github.com/donut2008/Calculator" target="_blank" rel="noopener noreferrer">Calculator (DEPRECATED)</Fluent.Button></li>
+					<li><Fluent.Button variant="hyperlink" href="https://github.com/donut2008/NeonX" target="_blank" rel="noopener noreferrer">NeonX (forked)</Fluent.Button></li>
+				</ul>
+			</svelte:fragment>
+		</Fluent.Expander>
+		</div>
+	<p>Still work in progress, expect more stuff to come soon!</p>
+</footer>
+
+<style>
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+	
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 40px;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 0px 40px 40px;
+		}
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,46 @@
+<script>
+	import * as Fluent from "fluent-svelte";
+	import "fluent-svelte/theme.css";
+	let dialogOpen = false;
+</script>
+
+<svelte:head>
+	<div class="head">
+		donut2008
+	</div>
+</svelte:head>
+<div class="pad">
+	<Fluent.TextBlock>Welcome to donut2008's homepage!</Fluent.TextBlock>
+</div>
+<div class="pad">
+<Fluent.Button variant="accent" href="https://github.com/donut2008" target="_blank" rel="noopener noreferrer">GitHub</Fluent.Button>
+&nbsp;
+<Fluent.Button variant="accent" href="https://t.me/donut2008" target="_blank" rel="noopener noreferrer">Telegram</Fluent.Button>
+</div>
+
+<style>
+	@import url("https://unpkg.com/fluent-svelte/theme.css");
+
+	.head{
+		font-size: 50px;
+		text-align: center;
+		font-weight: bold;
+	}
+	@media(prefers-color-scheme: light){
+		:root{
+			font-size: 16px;
+			text-align: center;
+			color: black;
+		}
+	}
+	@media(prefers-color-scheme: dark){
+		:root{
+			font-size: 16px;
+			text-align: center;
+			color: white;
+		}
+	}
+	.pad {
+		padding: 8px;
+	}
+</style>
