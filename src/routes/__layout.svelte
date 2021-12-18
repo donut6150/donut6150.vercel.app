39a9b0e7f49badbeb2a3ac9deae836c31514ffc0
donut2008 <76439683+donut2008@users.noreply.github.com>
2021-12-18 15:53:48 +0530

    __layout.svelte, index.svelte

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -12,59 +12,7 @@
 </main>
 
 <footer>
-	<div class="pad">
-		<Fluent.Expander>About me
-		<svelte:fragment slot="content">
-			<Fluent.TextBlock>(Pseudo?) developer learning C#, C++, JS, HTML, and CSS. Always open to friendly DMs.</Fluent.TextBlock>
-			<p></p>
-			<Fluent.TextBlock>You can find me here:
-				<ul>
-					<li>Discord - donut2008#7112</li>
-					<li>Telegram - @donut2008</li>
-					<li>GitHub - donut2008</li>
-				</ul>
-			</Fluent.TextBlock>
-		</svelte:fragment>
-	</Fluent.Expander>
-	</div>
-	<div class="pad">
-		<Fluent.Expander>GitHub projects
-			<svelte:fragment slot="content">
-				<ul>
-					<li><Fluent.Button variant="hyperlink" href="https://github.com/donut2008/Calculator" target="_blank" rel="noopener noreferrer">Calculator</Fluent.Button></li>
-					<li><Fluent.Button variant="hyperlink" href="https://github.com/donut2008/NeonX" target="_blank" rel="noopener noreferrer">NeonX</Fluent.Button></li>
-				</ul>
-			</svelte:fragment>
-		</Fluent.Expander>
-	</div>
-	<div class="pad">
-		<Fluent.Expander>Devices I have
-			<svelte:fragment slot="content">
-				<ul>
-					<li>HP 15s fr2006tu, specifications:<br>11th gen Intel core i3 1115G4<br>8 GB DDR4 RAM<br>15.6 inch 1080p IPS display<br>512 GB NVMe SSD</li>
-					<li>Samsung Galaxy M02 black (32/2 variant) running Android 11, rooted</li>
-					<li>Microsoft Lumia 540 running Windows 10 Mobile 1709</li>
-					<li>Logitech M185 wireless mouse</li>
-					<li>LG 32" 1080p TV from 2015</li>
-					<li>Sony MDR ZX110 headphones</li>
-				</ul>
-			</svelte:fragment>
-		</Fluent.Expander>
-	</div>
-	<div class="pad">
-		<Fluent.Expander>Credits
-			<svelte:fragment slot="content">
-				<ul>
-					<li><Fluent.Button variant="hyperlink" href="https://github.com/Tropix126" target="_blank" rel="noopener noreferrer">Tropix126</Fluent.Button></li>
-					<li><Fluent.Button variant="hyperlink" href="https://github.com/TheOnlyTails" target="_blank" rel="noopener noreferrer">TheOnlyTails</Fluent.Button></li>
-				</ul>
-				<p></p>
-				Amazing thanks for porting Microsoft's Fluent UI to Svelte!
-			</svelte:fragment>
-		</Fluent.Expander>
-	</div>
-	<p></p>
-	<div class="textalign">This website was built using Svelte, a web framework.</div>
+	
 </footer>
 
 <style>
