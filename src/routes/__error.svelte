76f8d265323e45e01fb2a916f7d00d732d25edff
donut2008 <76439683+donut2008@users.noreply.github.com>
2021-12-18 16:32:52 +0530

    Update __error.svelte

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -13,7 +13,7 @@
 <footer>
 	<div class="pad">
         <div class="textalign">
-            <img src="./error.gif" alt="error">
+            <img src="https://github.com/donut2008/donut2008.vercel.app/blob/master/src/routes/error.gif" alt="error">
         </div>
         <h1 class="ctext">Whoops, there seems to be an error...</h1>
     </div>
