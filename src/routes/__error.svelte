9310200004e38037071f106098df5fa6f491950c
donut2008 <76439683+donut2008@users.noreply.github.com>
2021-12-18 15:45:49 +0530

    Update __error.svelte

---
+++ src/routes/__error.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import Header from '$lib/header/Header.svelte';
+	import '../app.css';
+	import * as Fluent from "fluent-svelte";
+</script>
+
+<Header />
+
+<main>
+	<slot/>
+</main>
+
+<footer>
+	<div class="pad">
+        <div class="textalign">
+            <img src="./error.gif" alt="error">
+        </div>
+    </div>
+</footer>
+
+<style>
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+	
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: stretch;
+		align-items: stretch;
+		padding: 40px;
+	}
+	.textalign {
+		text-align: center;
+	}
+	@media (min-width: 480px) {
+		footer {
+			padding: 0px 40px 40px;
+		}
+	}
+</style>
