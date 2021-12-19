30cfec2f9d563799d220e3a2dddbe1c9b71cd39e
donut2008 <76439683+donut2008@users.noreply.github.com>
2021-12-19 13:52:33 +0530

    v0.2

--- src/routes/releases.svelte
+++ src/routes/releases.svelte
@@ -8,6 +8,24 @@
     <Fluent.TextBlock>Webpage containing all the releases of donut's calculator</Fluent.TextBlock>
 </div>
 <div class="pad">
+	<Fluent.Expander>Release 0.2 Public Beta
+		<svelte:fragment slot="content">
+			<Fluent.TextBlock>The first public release!<br>Changelog:
+				<ul>
+				<li>Fixed #1</li>
+				<li>New icon</li>
+				<li>New function: Square root</li>
+				<li>Added redistributables in case of DLL errors on launch</li>
+				</ul>Keep in mind that this is a public beta release, so you may encounter bugs. Have fun :)</Fluent.TextBlock>
+			<div class="ctext">
+				<Fluent.Button variant="accent" href="https://github.com/donut2008/Calculator/releases/download/v0.2_public-beta/v0.2_public-beta.zip">Download</Fluent.Button>
+				<Fluent.Button href="https://github.com/donut2008/Calculator/archive/refs/tags/v0.2_public-beta.zip">Source code (zip)</Fluent.Button>
+				<Fluent.Button href="https://github.com/donut2008/Calculator/archive/refs/tags/v0.2_public-beta.tar.gz">Source code (tar.gz)</Fluent.Button>
+			</div>
+		</svelte:fragment>
+	</Fluent.Expander>
+</div>
+<div class="pad">
     <Fluent.Expander>[PRE-RELEASE] 0.1 Alpha
 		<svelte:fragment slot="content">
 			<Fluent.TextBlock>The first release of this CLI calculator! Have fun :)</Fluent.TextBlock><br>
