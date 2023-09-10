a57d19ae2553761610905fb4a16f59efe655a850
donut2008 <76439683+donut2008@users.noreply.github.com>
2023-09-10 23:11:55 +0530

    Update NeonX.svelte

--- src/routes/NeonX.svelte
+++ src/routes/NeonX.svelte
@@ -3,6 +3,5 @@
 	import "fluent-svelte/theme.css";
 </script>
 <h1>
-	how the hell do you display html in svelte
-	code is in html not in svelte
+	how the hell do you display html in svelte; the code is in html not in svelte
 </h1>
