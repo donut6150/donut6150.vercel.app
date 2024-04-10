2e32d9336aaeb6c0fd2423ca2745cfc06c1aee32
donut <donut@DONUTSTATION>
2024-04-10 16:39:19 +0530

    Update error icon

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,71 +1,78 @@
 <script lang="ts">
-	import '../app.css';
-	import * as Fluent from "fluent-svelte";
-    </script>
+    import '../app.css';
+    import {TextBlock} from "fluent-svelte";
+</script>
 
 <main>
-	<slot/>
+    <slot/>
 </main>
 
 <footer>
-	<div class="pad">
+    <div class="pad">
         <div class="textalign">
-            <img src="https://cdn.discordapp.com/attachments/892316599661518872/921788638554124308/error.ico" alt="error">
+            <img src="/img/error.ico" alt="error">
         </div>
         <h1 class="ctext">Whoops, there seems to be an error...</h1>
     </div>
     <div class="pad">
-		<div class="ctext">
-        <Fluent.TextBlock>The requested page either does not exist, or didn't load correctly.</Fluent.TextBlock>
-	</div>
+        <div class="ctext">
+            <TextBlock>The requested page either does not exist, or didn't load correctly.</TextBlock>
+        </div>
     </div>
 </footer>
 
 <style>
     @import url("https://unpkg.com/fluent-svelte/theme.css");
-	@media(prefers-color-scheme: light){
-		:root{
-			font-size: 16px;
-			text-align: left;
-			color: black;
-		}
-	}
-	@media(prefers-color-scheme: dark){
-		:root{
-			font-size: 16px;
-			text-align: left;
-			color: white;
-		}
-	}
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-	
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: stretch;
-		align-items: stretch;
-		padding: 40px;
-	}
-	.textalign {
-		text-align: center;
-	}
-	@media (min-width: 480px) {
-		footer {
-			padding: 0px 40px 40px;
-		}
-	}
-	h1 {
-		font-size: 40px;
-	}
+
+    @media (prefers-color-scheme: light) {
+        :root {
+            font-size: 16px;
+            text-align: left;
+            color: black;
+        }
+    }
+
+    @media (prefers-color-scheme: dark) {
+        :root {
+            font-size: 16px;
+            text-align: left;
+            color: white;
+        }
+    }
+
+    main {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        padding: 1rem;
+        width: 100%;
+        margin: 0 auto;
+        box-sizing: border-box;
+    }
+
+    footer {
+        display: flex;
+        flex-direction: column;
+        justify-content: stretch;
+        align-items: stretch;
+        padding: 40px;
+    }
+
+    .textalign {
+        text-align: center;
+    }
+
+    @media (min-width: 480px) {
+        footer {
+            padding: 0 40px 40px;
+        }
+    }
+
+    h1 {
+        font-size: 40px;
+    }
+
     .ctext {
-		text-align: center;
-	}
+        text-align: center;
+    }
 </style>
