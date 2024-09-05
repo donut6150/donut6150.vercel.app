78aff1b0a8f9a96505d30f5661c8332e1b9720a1
donut2008 <76439683+donut2008@users.noreply.github.com>
2024-09-05 17:16:28 +0530

    Even more updates

--- src/routes/UWPDrawingCanvas/+page.svelte
+++ src/routes/UWPDrawingCanvas/+page.svelte
@@ -32,35 +32,3 @@
         </svelte:fragment>
     </Fluent.Expander>
 </div>
-<style>
-	@import url("https://unpkg.com/fluent-svelte/theme.css");
-	@media(prefers-color-scheme: light)
-	{
-		:root
-		{
-			font-size: 16px;
-			text-align: left;
-			color: black;
-		}
-	}
-	@media(prefers-color-scheme: dark)
-	{
-		:root
-		{
-			font-size: 16px;
-			text-align: left;
-			color: white;
-		}
-	}
-	.pad
-	{
-		padding: 8px;
-	}
-	.ctext
-	{
-		text-align: center;
-	}
-	h1 {
-		font-size: 40px;
-	}
-</style>
