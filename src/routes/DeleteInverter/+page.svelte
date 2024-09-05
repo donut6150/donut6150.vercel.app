fa6b495cafd9aac81c5969097c01ad401314cea6
donut2008 <76439683+donut2008@users.noreply.github.com>
2024-09-05 17:43:03 +0530

    Update more pages

--- src/routes/DeleteInverter/+page.svelte
+++ src/routes/DeleteInverter/+page.svelte
@@ -10,35 +10,3 @@
 <div class="pad ctext">
     <TextBlock>status</TextBlock>
 </div>
-<style>
-    @import url("https://unpkg.com/fluent-svelte/theme.css");
-	@media(prefers-color-scheme: light)
-	{
-		:root
-		{
-			font-size: 16px;
-			text-align: left;
-			color: black;
-		}
-	}
-	@media(prefers-color-scheme: dark)
-	{
-		:root
-		{
-			font-size: 16px;
-			text-align: left;
-			color: white;
-		}
-	}
-	.pad
-	{
-		padding: 8px;
-	}
-	.ctext
-	{
-		text-align: center;
-	}
-	h1 {
-		font-size: 40px;
-	}
-</style>
