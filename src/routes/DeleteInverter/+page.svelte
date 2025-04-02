071f0acb884113ad8cada5bec4921cd170c393ab
donut2008 <76439683+donut2008@users.noreply.github.com>
2025-04-02 10:37:40 +0530

    Add DeleteInverter info

--- src/routes/DeleteInverter/+page.svelte
+++ src/routes/DeleteInverter/+page.svelte
@@ -1,12 +1,36 @@
 <script>
-    import { TextBlock } from "fluent-svelte";
+    import { TextBlock, Button, Expander } from "fluent-svelte";
     import "fluent-svelte/theme.css";
 </script>
-
 <h1 class="ctext">DeleteInverter</h1>
 <div class="pad ctext">
-    <TextBlock>something something abcd</TextBlock>
+    <TextBlock>An app which deletes files except the ones you manually select. Exclusions will not be deleted.</TextBlock>
 </div>
 <div class="pad ctext">
-    <TextBlock>status</TextBlock>
+    <Button variant="accent" href="https://github.com/donut2008/DeleteInverter/releases/download/1.1.0/DeleteInverter_1.1.0.0.zip">Download latest build</Button>
+    <Button href="DeleteInverter/releases" target="blank" rel="noopener noreferrer">View all releases</Button>
+</div>
+<div class="pad">
+    <Expander>Screenshots (light mode)
+        <svelte:fragment slot="content">
+            <table>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </table>
+        </svelte:fragment>
+    </Expander>
+    <Expander>Screenshots (dark mode)
+        <svelte:fragment slot="content">
+            <table>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </table>
+        </svelte:fragment>
+    </Expander>
 </div>
---
+++ src/routes/DeleteInverter/releases/+page.svelte
@@ -0,0 +1,45 @@
+<script>
+	import { TextBlock, Expander, Button } from "fluent-svelte"; 
+	import "fluent-svelte/theme.css";
+</script>
+<h1 class="ctext">DeleteInverter: releases</h1>
+<div class="pad ctext">
+	<TextBlock>All releases of DeleteInverter</TextBlock>
+</div>
+<div class="pad">
+	<Expander>
+		<TextBlock variant="subtitle">1.1.0</TextBlock>
+		<br><br>
+		<TextBlock variant="caption">Released on: Feb 21, 2025</TextBlock>
+		<svelte:fragment slot="content">
+			<TextBlock>
+                <ul>
+                    <li>Fixed crash on attempting to delete files</li>
+                    <li>Added more info in help</li>
+                </ul>
+            </TextBlock>
+			<div class="pad ctext">
+				<Button variant="accent" href="https://github.com/donut2008/DeleteInverter/releases/download/1.1.0/DeleteInverter_1.1.0.0.zip">Download</Button>
+				<Button href="https://github.com/donut2008/DeleteInverter/archive/refs/tags/1.1.0.zip">Source code (zip)</Button>
+				<Button href="https://github.com/donut2008/DeleteInverter/archive/refs/tags/1.1.0.tar.gz">Source code (tar.gz)</Button>
+			</div>
+		</svelte:fragment>
+	</Expander>
+</div>
+<div class="pad">
+	<Expander>
+		<TextBlock variant="subtitle">1.0.0</TextBlock>
+        <br><br>
+        <TextBlock variant="caption">Released on: Aug 26, 2024</TextBlock>
+		<svelte:fragment slot="content">
+			<TextBlock>Initial release!</TextBlock>
+            <div class="pad ctext">
+                <Button variant="accent" href="https://github.com/donut2008/DeleteInverter/releases/download/1.0.0/DeleteInverter_1.0.0.0_ARM.appx">Download (ARM)</Button>
+                <Button variant="accent" href="https://github.com/donut2008/DeleteInverter/releases/download/1.0.0/DeleteInverter_1.0.0.0_x64.appx">Download (x64)</Button>
+                <Button variant="accent" href="https://github.com/donut2008/DeleteInverter/releases/download/1.0.0/DeleteInverter_1.0.0.0_x86.appx">Download (x86)</Button>
+                <Button href="https://github.com/donut2008/DeleteInverter/archive/refs/tags/1.0.0.zip">Source code (zip)</Button>
+                <Button href="https://github.com/donut2008/DeleteInverter/archive/refs/tags/1.0.0.tar.gz">Source code (tar.gz)</Button>
+            </div>
+        </svelte:fragment>
+    </Expander>
+</div>
