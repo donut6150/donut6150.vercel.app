7b860877d54f8c5a2a34e7cb432f8abb78294497
donut2008 <76439683+donut2008@users.noreply.github.com>
2025-06-03 09:26:03 +0530

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -39,9 +39,13 @@
 	<div class="card">
 		<h2 class="pad bgs">GitHub statistics</h2>
 		<div class="pad">
-			<img src="https://github-readme-stats.vercel.app/api?username=donut2008&show_icons=true&theme=dark"/>
-			<img src="https://github-readme-streak-stats.herokuapp.com?user=donut2008&theme=dark">
-			<img src="https://github-readme-stats.vercel.app/api/top-langs/?username=donut2008&theme=dark" alt="Most used languages">
+			<table>
+				<tr>
+					<td><img src="https://github-readme-stats.vercel.app/api?username=donut2008&show_icons=true&theme=dark"/></td>
+					<td><img src="https://github-readme-streak-stats.herokuapp.com?user=donut2008&theme=dark"></td>
+					<td><img src="https://github-readme-stats.vercel.app/api/top-langs/?username=donut2008&theme=dark" alt="Most used languages"></td>
+				</tr>
+			</table>
 		</div>
 	</div>
 	<div class="card">
