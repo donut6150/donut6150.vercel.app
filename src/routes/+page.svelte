a044edcae7cb6e4e4de7060ffe33f4b63ac1096e
donut2008 <76439683+donut2008@users.noreply.github.com>
2025-06-06 14:07:54 +0530

    Add alt text to some images
    Signed-off-by: donut2008 <76439683+donut2008@users.noreply.github.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -41,8 +41,8 @@
 		<div class="pad">
 			<table>
 				<tr>
-					<td><img src="https://github-readme-stats.vercel.app/api?username=donut2008&show_icons=true&theme=dark"/></td>
-					<td><img src="https://github-readme-streak-stats.herokuapp.com?user=donut2008&theme=dark"></td>
+					<td><img src="https://github-readme-stats.vercel.app/api?username=donut2008&show_icons=true&theme=dark" alt="GitHub stats"/></td>
+					<td><img src="https://github-readme-streak-stats.herokuapp.com?user=donut2008&theme=dark" alt="Streak"></td>
 					<td><img src="https://github-readme-stats.vercel.app/api/top-langs/?username=donut2008&theme=dark" alt="Most used languages"></td>
 				</tr>
 			</table>
