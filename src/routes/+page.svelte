7fef062e17f77cc2e72c80f85f3268467601de44
donut6150 <76439683+donut6150@users.noreply.github.com>
2025-08-13 11:56:03 +0530

    src: Fix horizontal scrolling in some sections
    Using tables
    
    Signed-off-by: donut6150 <76439683+donut6150@users.noreply.github.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,35 +1,32 @@
 <script lang="ts">
 </script>
+
 <div class="main">
 	<h1 class="ctext">donut6150</h1>
-	<div class="pad ctext">
-		Welcome to donut6150's website!
-	</div>
+	<div class="pad ctext">Welcome to donut6150's website!</div>
 	<sl-card class="card-header">
-  		<div slot="header">
+		<div slot="header">
 			<h2>About me</h2>
 		</div>
 		<div style="font-size: var(--sl-font-size-medium);">
 			Someone learning C, C#, C++, Java, Python, and TypeScript. I plan to use Rust as well, soon.
 			<br />
-			I'm somewhat of a musician, although I don't make music.
-			I play the keyboards, and have a Yamaha PSR-I455 for the purpose. Taking a music
-			break really helps when it comes to any task, and that's why I love music.
+			I'm somewhat of a musician, although I don't make music. I play the keyboards, and have a Yamaha
+			PSR-I455 for the purpose. Taking a music break really helps when it comes to any task, and that's
+			why I love music.
 			<br />
-			Why donut6150 though? The "donut" is completely random - I had no
-			idea what to use as a username, and for some reason "donut" sounded
-			really appropriate. "6150" is a reference to the Snapdragon 675 SoC.
+			Why donut6150 though? The "donut" is completely random - I had no idea what to use as a username,
+			and for some reason "donut" sounded really appropriate. "6150" is a reference to the Snapdragon
+			675 SoC.
 			<br />
-			I'm usually active on Discord when it comes to development. Feel
-			free to join the Developer Sanctuary Discord server and check out
-			the #donut-projects channel, if you want to see what I'm working
-			on next!<br /><br />
+			I'm usually active on Discord when it comes to development. Feel free to join the Developer Sanctuary
+			Discord server and check out the #donut-projects channel, if you want to see what I'm working on
+			next!<br /><br />
 			<sl-button
 				href="https://discord.gg/windows-apps-hub-714581497222398064"
 				variant="primary"
 				target="_blank"
-				rel="noopener noreferrer"
-				>Invite link (permanently valid)</sl-button
+				rel="noopener noreferrer">Invite link (permanently valid)</sl-button
 			>
 		</div>
 	</sl-card>
@@ -42,19 +39,26 @@
 				<tbody>
 					<tr>
 						<td>
-							<img src="https://github-readme-stats.vercel.app/api?username=donut6150&show_icons=true&theme=transparent"
-							alt="GitHub stats"
-							style="width: auto !important; height: auto !important;"/>
+							<img
+								src="https://github-readme-stats.vercel.app/api?username=donut6150&show_icons=true&theme=transparent"
+								alt="GitHub stats"
+								style="width: auto !important; height: auto !important;"
+							/>
 						</td>
 						<td>
-							<img src="https://github-readme-streak-stats-gray-one.vercel.app?user=donut6150&theme=transparent"
-							alt="GitHub Streak"
-							style="width: auto !important; height: auto !important;"/>
+							<img
+								src="https://github-readme-streak-stats-gray-one.vercel.app?user=donut6150&theme=transparent"
+								alt="GitHub Streak"
+								style="width: auto !important; height: auto !important;"
+							/>
 						</td>
 						<td>
-							<img src="https://github-readme-stats.vercel.app/api/top-langs/?username=donut6150&theme=transparent&layout=compact"
-							alt="Most used languages"
-							style="width: auto !important; height: auto !important;"/></td>
+							<img
+								src="https://github-readme-stats.vercel.app/api/top-langs/?username=donut6150&theme=transparent&layout=compact"
+								alt="Most used languages"
+								style="width: auto !important; height: auto !important;"
+							/></td
+						>
 					</tr>
 				</tbody>
 			</table>
@@ -64,43 +68,51 @@
 		<div slot="header">
 			<h2>Kernels</h2>
 		</div>
-		<div>
-			<sl-button outline
-				style="margin: 2px 0;"
-				variant="primary"
-				href="https://github.com/donut6150/kernel_samsung_universal7904"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				<figure style="margin: 0;">
-					<img
-						src="https://raw.githubusercontent.com/donut6150/donut6150.vercel.app/master/img/A30s.png"
-						alt="Samsung Galaxy A30s"
-						style="width: 128px !important; height: 128px !important;"
-					/>
-					<figcaption class="ctext">
-						Samsung Galaxy A30s (universal7904, 4.4.177)
-					</figcaption>
-				</figure>
-			</sl-button>
-			<sl-button outline
-				style="margin: 2px 0;"
-				variant="primary"
-				href="https://github.com/donut6150/kernel_samsung_sm6150"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				<figure style="margin: 0;">
-					<img
-						src="https://raw.githubusercontent.com/donut6150/donut6150.vercel.app/master/img/M40.png"
-						alt="Samsung Galaxy M40"
-						style="width: 128px !important; height: 128px !important;"
-					/>
-					<figcaption class="ctext">
-						Samsung Galaxy M40 (sm6150, 4.14.190)
-					</figcaption>
-				</figure>
-			</sl-button>
+		<div style="overflow-x: auto;">
+			<table>
+				<tbody>
+					<tr>
+						<td>
+							<sl-button
+								outline
+								variant="primary"
+								href="https://github.com/donut6150/kernel_samsung_universal7904"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<figure style="margin: 0;">
+									<img
+										src="https://raw.githubusercontent.com/donut6150/donut6150.vercel.app/master/img/A30s.png"
+										alt="Samsung Galaxy A30s"
+										style="width: 128px !important; height: 128px !important;"
+									/>
+									<figcaption class="ctext">
+										Samsung Galaxy A30s (universal7904, 4.4.177)
+									</figcaption>
+								</figure>
+							</sl-button>
+						</td>
+						<td>
+							<sl-button
+								outline
+								variant="primary"
+								href="https://github.com/donut6150/kernel_samsung_sm6150"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<figure style="margin: 0;">
+									<img
+										src="https://raw.githubusercontent.com/donut6150/donut6150.vercel.app/master/img/M40.png"
+										alt="Samsung Galaxy M40"
+										style="width: 128px !important; height: 128px !important;"
+									/>
+									<figcaption class="ctext">Samsung Galaxy M40 (sm6150, 4.14.190)</figcaption>
+								</figure>
+							</sl-button>
+						</td>
+					</tr>
+				</tbody>
+			</table>
 		</div>
 	</sl-card>
 	<sl-card class="card-header">
@@ -108,45 +120,66 @@
 			<h2>GitHub projects</h2>
 		</div>
 		<div style="overflow-x: auto;">
-			<sl-button outline
-				variant="primary"
-				href="Calculator"
-				target="_blank"
-				rel="noopener noreferrer">Calculator</sl-button
-			>
-			<sl-button outline
-				variant="primary"
-				href="https://neonx.vercel.app"
-				target="_blank"
-				rel="noopener noreferrer">NeonX</sl-button
-			>
-			<sl-button outline
-				variant="primary"
-				href="UWPDrawingCanvas"
-				target="_blank"
-				rel="noopener noreferrer"
-				>Drawing Canvas UWP</sl-button
-			>
-			<sl-button outline
-				variant="primary"
-				href="DonutOS"
-				target="_blank"
-				rel="noopener noreferrer">DonutOS</sl-button
-			>
-			<sl-button outline
-				variant="primary"
-				href="WinTranslate"
-				target="_blank"
-				rel="noopener noreferrer"
-				>WinTranslate</sl-button
-			>
-			<sl-button outline
-				variant="primary"
-				href="DeleteInverter"
-				target="_blank"
-				rel="noopener noreferrer"
-				>DeleteInverter</sl-button
-			>
+			<table>
+				<tbody>
+					<tr>
+						<td>
+							<sl-button
+								outline
+								variant="primary"
+								href="Calculator"
+								target="_blank"
+								rel="noopener noreferrer">Calculator</sl-button
+							>
+						</td>
+						<td>
+							<sl-button
+								outline
+								variant="primary"
+								href="https://neonx.vercel.app"
+								target="_blank"
+								rel="noopener noreferrer">NeonX</sl-button
+							>
+						</td>
+						<td>
+							<sl-button
+								outline
+								variant="primary"
+								href="UWPDrawingCanvas"
+								target="_blank"
+								rel="noopener noreferrer">Drawing Canvas UWP</sl-button
+							>
+						</td>
+						<td>
+							<sl-button
+								outline
+								variant="primary"
+								href="DonutOS"
+								target="_blank"
+								rel="noopener noreferrer">DonutOS</sl-button
+							>
+						</td>
+						<td>
+							<sl-button
+								outline
+								variant="primary"
+								href="WinTranslate"
+								target="_blank"
+								rel="noopener noreferrer">WinTranslate</sl-button
+							>
+						</td>
+						<td>
+							<sl-button
+								outline
+								variant="primary"
+								href="DeleteInverter"
+								target="_blank"
+								rel="noopener noreferrer">DeleteInverter</sl-button
+							>
+						</td>
+					</tr>
+				</tbody>
+			</table>
 		</div>
 	</sl-card>
 	<sl-card class="card-header">
@@ -154,86 +187,97 @@
 			<h2>Socials</h2>
 		</div>
 		<div style="overflow-x: auto;">
-			<sl-button outline
-				variant="primary"
-				href="https://telegram.me/donut6150"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				<figure style="margin: 0;">
-					<img
-						src="https://raw.githubusercontent.com/donut6150/donut6150.vercel.app/master/img/telegram.svg"
-						alt="Telegram"
-					/>
-					<figcaption class="ctext">
-						donut6150
-					</figcaption>
-				</figure>
-			</sl-button>
-			<sl-button outline
-				variant="primary"
-				href="https://discord.com/users/796351855181037569"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				<figure style="margin: 0;">
-					<img
-						src="https://raw.githubusercontent.com/donut6150/donut6150.vercel.app/master/img/discord.svg"
-						alt="Discord"
-					/>
-					<figcaption class="ctext">
-						donut.6150
-					</figcaption>
-				</figure>
-			</sl-button>
-			<sl-button outline
-				variant="primary"
-				href="https://github.com/donut6150"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				<figure style="margin: 0;">
-					<img
-						src="https://raw.githubusercontent.com/donut6150/donut6150.vercel.app/master/img/github.svg"
-						alt="Github"
-					/>
-					<figcaption class="ctext">
-						donut6150
-					</figcaption>
-				</figure>
-			</sl-button>
-			<sl-button outline
-				variant="primary"
-				href="https://youtube.com/@WalnutException"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				<figure style="margin: 0;">
-					<img
-						src="https://raw.githubusercontent.com/donut6150/donut6150.vercel.app/master/img/youtube.svg"
-						alt="YouTube"
-					/>
-					<figcaption class="ctext">
-						@WalnutException
-					</figcaption>
-				</figure>
-			</sl-button>
-			<sl-button outline
-				variant="primary"
-				href="http://live.xbox.com/Profile?Gamertag=FireFox44039"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				<figure style="margin: 0;">
-					<img
-						src="https://raw.githubusercontent.com/donut6150/donut6150.vercel.app/master/img/xbox.svg"
-						alt="Xbox"
-					/>
-					<figcaption class="ctext">
-						FireFox44039
-					</figcaption>
-				</figure>
-			</sl-button>
+			<table>
+				<tbody>
+					<tr>
+						<td>
+							<sl-button
+								outline
+								variant="primary"
+								href="https://telegram.me/donut6150"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<figure style="margin: 0;">
+									<img
+										src="https://raw.githubusercontent.com/donut6150/donut6150.vercel.app/master/img/telegram.svg"
+										alt="Telegram"
+									/>
+									<figcaption class="ctext">donut6150</figcaption>
+								</figure>
+							</sl-button>
+						</td>
+						<td>
+							<sl-button
+								outline
+								variant="primary"
+								href="https://discord.com/users/796351855181037569"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<figure style="margin: 0;">
+									<img
+										src="https://raw.githubusercontent.com/donut6150/donut6150.vercel.app/master/img/discord.svg"
+										alt="Discord"
+									/>
+									<figcaption class="ctext">donut.6150</figcaption>
+								</figure>
+							</sl-button>
+						</td>
+						<td>
+							<sl-button
+								outline
+								variant="primary"
+								href="https://github.com/donut6150"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<figure style="margin: 0;">
+									<img
+										src="https://raw.githubusercontent.com/donut6150/donut6150.vercel.app/master/img/github.svg"
+										alt="Github"
+									/>
+									<figcaption class="ctext">donut6150</figcaption>
+								</figure>
+							</sl-button>
+						</td>
+						<td>
+							<sl-button
+								outline
+								variant="primary"
+								href="https://youtube.com/@WalnutException"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<figure style="margin: 0;">
+									<img
+										src="https://raw.githubusercontent.com/donut6150/donut6150.vercel.app/master/img/youtube.svg"
+										alt="YouTube"
+									/>
+									<figcaption class="ctext">@WalnutException</figcaption>
+								</figure>
+							</sl-button>
+						</td>
+						<td>
+							<sl-button
+								outline
+								variant="primary"
+								href="http://live.xbox.com/Profile?Gamertag=FireFox44039"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<figure style="margin: 0;">
+									<img
+										src="https://raw.githubusercontent.com/donut6150/donut6150.vercel.app/master/img/xbox.svg"
+										alt="Xbox"
+									/>
+									<figcaption class="ctext">FireFox44039</figcaption>
+								</figure>
+							</sl-button>
+						</td>
+					</tr>
+				</tbody>
+			</table>
 		</div>
 	</sl-card>
 </div>
